db263fb469267fbc5004f244e2e9e5ab2ac685ef
Jirayu <jirayu.ko@tbn.co.th>
2026-07-19 16:27:15 +0700

    Fix Android notification status icon

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,13 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.runmate.mobile',
   appName: 'RunMate',
-  webDir: 'dist'
+  webDir: 'dist',
+  plugins: {
+    LocalNotifications: {
+      smallIcon: 'ic_stat_runmate',
+      iconColor: '#2F94D0'
+    }
+  }
 };
 
 export default config;
